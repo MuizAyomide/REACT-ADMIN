d1dc3a6032f47766247a24e31f0ff3a2155d6d95
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-18 00:57:40 +0100

    app commit

---
+++ react-admin/src/App.tsx
@@ -0,0 +1,44 @@
+import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./pages/Users/Users";
+import Products from "./pages/Products/Products";
+
+
+
+function App() {
+
+  const Layout = () =>{
+    return(
+      <div className='main'>
+      </div>
+    )
+    
+  }
+
+  const router = createBrowserRouter([
+    {
+          path: "/",
+          element: <Home/ >,
+    },
+    {
+            path: "users",
+          element: <Users />,
+    },
+    {
+            path: "products",
+          element: <Products />,
+    },
+    
+          ]);
+
+          return  <RouterProvider router={router}/>
+
+          
+}
+
+          export default App
