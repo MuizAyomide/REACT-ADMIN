6adbcd2607f0da414fba9e66e4374d0a3908b194
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-18 19:40:15 +0100

    commit

--- react-admin/src/Components/Navbar/Navbar.tsx
+++ react-admin/src/Components/Navbar/Navbar.tsx
@@ -3,7 +3,25 @@ import './Navbar.scss'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      Nav
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>muizadmin</span>
+      </div>
+      <div className="links">
+        <img src="search.svg" alt="" className="icon" />
+        <img src="app.svg" alt="" className="icon" />
+        <img src="expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="notification.svg" alt="" /><span>1</span>
+        </div>
+        <div className="user">
+          <img src="image" alt="" />
+          <span>Muiz</span>
+        </div>
+
+        <img src="settings.svg" alt="" className="icon" />
+
+      </div>
     </div>
   )
 }
