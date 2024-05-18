9b9ccf5a78ecf68b30c09a96b85b46c1a8ded6e0
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-18 20:54:29 +0100

    commit

--- react-admin/src/Components/Menu/Menu.tsx
+++ react-admin/src/Components/Menu/Menu.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Menu.scss'
+import { Link } from 'react-router-dom'
 const Menu = () => {
   return (
     <div className='menu'>
-      Menu
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to='/' className='listItem'>
+        <img src="home.svg" alt="" />
+        <span className='listitemTitle'>Home</span>
+        </Link>
+        <Link to='/' className='listItem'>
+        <img src="profile.svg" alt="" />
+        <span className='listitemTitle'>Profile</span>
+        </Link>
+      </div>
+      <div className="item">
+      <span className="title">MAIN</span>
+        <Link to='/' className='listItem'>
+        <img src="home.svg" alt="" />
+        <span className='listitemTitle'>Home</span>
+        </Link>
+        <Link to='/' className='listItem'>
+        <img src="profile.svg" alt="" />
+        <span className='listitemTitle'>Home</span>
+        </Link>
+      </div>
+      
     </div>
   )
 }
