7961623828e6053c45b4f575b08766f9fca3676e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-19 23:11:52 +0100

    commit

--- react-admin/src/Components/BArChartBox/BarChartBox.tsx
+++ react-admin/src/Components/BArChartBox/BarChartBox.tsx
@@ -2,10 +2,10 @@ import './BarChartBox.scss'
 import { Bar, BarChart, ResponsiveContainer, Tooltip } from 'recharts'
 
 type Props = {
-    color: string;
-    title: string
-    dataKey: string
-    chartData: object[]
+  color: string;
+  title: string
+  dataKey: string
+  chartData: object[]
 }
 
 const BarChartBox = (props: Props) => {
@@ -13,16 +13,16 @@ const BarChartBox = (props: Props) => {
     <div className='barchartbox'>
       <h1>{props.title}</h1>
       <div className="chart">
-      <ResponsiveContainer width="99%" height={150}>
-        <BarChart data={props.chartData}>
+        <ResponsiveContainer width="99%" height={150}>
+          <BarChart data={props.chartData}>
             <Tooltip
-            contentStyle={{background:'#2a3447', borderRadius: '5px'}}
-            // labelStyle={{display: 'none'}}
-            cursor={{fill:'none'}}
+              contentStyle={{ background: '#2a3447', borderRadius: '5px' }}
+              // labelStyle={{display: 'none'}}
+              cursor={{ fill: 'none' }}
             />
-          <Bar dataKey={props.dataKey} fill={props.color} />
-        </BarChart>
-      </ResponsiveContainer>
+            <Bar dataKey={props.dataKey} fill={props.color} />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     </div>
   )
