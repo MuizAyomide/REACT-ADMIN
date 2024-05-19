c8e3740bba52d199d6f60054de020443bd0ad28a
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-19 23:08:00 +0100

    commit

--- react-admin/src/pages/Users/Users.tsx
+++ react-admin/src/pages/Users/Users.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import DataTable from '../../Components/DataTable/DataTable'
 
 const Users = () => {
   return (
     <div className='users'>
-      
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add new user</button>
+      </div>
+      <DataTable/>
     </div>
   )
 }
