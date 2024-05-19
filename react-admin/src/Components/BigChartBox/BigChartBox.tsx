dcd7a7634afeac32c198b72362fea683e4be175e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-19 01:52:07 +0100

    commit

--- react-admin/src/Components/BigChartBox/BigChartBox.tsx
+++ react-admin/src/Components/BigChartBox/BigChartBox.tsx
@@ -66,7 +66,7 @@ const BigChartBox = () => {
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Area type="monotone" dataKey="electronic" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="electronics" stackId="1" stroke="#8884d8" fill="#8884d8" />
           <Area type="monotone" dataKey="clothes" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
           <Area type="monotone" dataKey="books" stackId="1" stroke="#ffc658" fill="#ffc658" />
         </AreaChart>
