0491d2b220baa875b7724bafc59966c5fb84d983
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-21 18:41:21 +0100

    commit

---
+++ react-admin/src/pages/User/User.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './User.scss'
+import Single from '../../Components/Single/Single'
+
+const User = () => {
+
+        //fetch data and fetch to single component
+  return (
+    <div className='user'>
+      <Single/>
+    </div>
+  )
+}
+
+export default User
