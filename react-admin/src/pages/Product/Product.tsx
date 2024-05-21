d8a05d0574b13775c34d8d9932fee1770358d220
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-21 18:41:09 +0100

    commit

---
+++ react-admin/src/pages/Product/Product.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Product.scss'
+
+const Product = () => {
+
+    //fetch data and fetch to single component
+  return (
+    <div className='product'>
+      sdfvbnm,
+    </div>
+  )
+}
+
+export default Product
