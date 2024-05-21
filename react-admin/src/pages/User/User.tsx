de1095940eb72e72fe4175a2643304a995bae9b4
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-21 23:40:52 +0100

    commit

--- react-admin/src/pages/User/User.tsx
+++ react-admin/src/pages/User/User.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './User.scss'
 import Single from '../../Components/Single/Single'
+import { singleUser } from '../../data'
 
 const User = () => {
 
         //fetch data and fetch to single component
   return (
     <div className='user'>
-      <Single/>
+      <Singl {...singleUser}/>
     </div>
   )
 }
