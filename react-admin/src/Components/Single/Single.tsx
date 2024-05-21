4e1ed451f3f0d323cba181b259da1efb0cc9d4c0
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-21 18:40:46 +0100

    commit

--- react-admin/src/Components/Single/Single.tsx
+++ react-admin/src/Components/Single/Single.tsx
@@ -3,7 +3,24 @@ import './Single.scss'
 const Single = () => {
   return (
     <div className='single'>
-      
+        <div className="view">
+<div className="info">
+    <div className="top-info">
+        <img src="" alt="" />
+        <h1>John Doe</h1>
+        <button>Update</button>
+    </div>
+    <div className="details">
+        <div className="item">
+            <span className="item-title">Username:</span>
+            <span className="item-value">John Doe</span>
+        </div>
+    </div>
+</div>
+<div className="chart"></div>
+
+        </div>
+      <div className="activities"></div>
     </div>
   )
 }
